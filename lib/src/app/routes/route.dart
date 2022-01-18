2f764f4a189be278922070588dac0e88812e003a
Thio Alli <connect.thioalli@gmail.com>
2022-01-18 15:19:03 +0700

    feat : route - route.dart

---
+++ lib/src/app/routes/route.dart
@@ -0,0 +1,45 @@
+class Routes {
+  // Splash Screen
+  static const splash = '/';
+
+  // OnBoarding Screen
+  static const onBoard = '/onboard';
+
+  // Sign Up Screen
+  static const signUp = '/signup';
+  static const signUpAuth = '/signup/auth';
+  static const signUpSuccess = '/signup/auth/success';
+
+  // Sign In Screen
+  static const signIn = '/signin';
+  static const forgot = '/signin/forgot';
+  static const forgotAuth = '/signin/forgot/auth';
+  static const reset = '/signin/reset';
+
+  // Sign Up Screen
+  static const signOut = '/signout';
+
+  // Terms & Conditions Screen
+  static const terms = '/terms';
+
+  // Privacy Policy Screen
+  static const privacy = '/privacy';
+
+  // Home Screen
+  static const home = '/app/home';
+
+  // Favorites Screen
+  static const favorites = '/app/favorites';
+
+  // Messages Screen
+  static const messages = '/app/messages';
+
+  // Profile Screen
+  static const profile = '/app/profile';
+  static const profileDetails = '/app/profile/details';
+  static const notifications = '/app/profile/notifications';
+  static const setting = '/app/profile/settings';
+  static const support = '/app/profile/support';
+  static const help = '/app/profile/help';
+  static const about = '/app/profile/about';
+}
