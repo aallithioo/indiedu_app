8f22204f8db2a31d9a04f01d5f829c7f277f0407
Thio Alli <connect.thioalli@gmail.com>
2022-01-19 14:37:45 +0700

    feat : upper body - splash_body.dart

---
+++ lib/src/screens/splash/widgets/splash_body.dart
@@ -0,0 +1,65 @@
+import 'package:aallithioo/src/app/widgets/custom_padding.dart';
+import 'package:aallithioo/src/screens/splash/widgets/splash_content.dart';
+import 'package:flutter/material.dart';
+
+class SplashBody extends StatefulWidget {
+  const SplashBody({Key? key}) : super(key: key);
+
+  @override
+  _SplashBodyState createState() => _SplashBodyState();
+}
+
+class _SplashBodyState extends State<SplashBody> {
+  int currentPage = 0;
+  List<Map<String, String>> splashData = [
+    {
+      "text": "Welcome to Tooko, Let's shop!",
+      "image": "assets/images/png/img_dummy_00.png",
+    },
+    {
+      "text": "We help people to find the best products",
+      "image": "assets/images/png/img_dummy_00.png",
+    },
+    {
+      "text": "We show the easy way to buy",
+      "image": "assets/images/png/img_dummy_00.png",
+    },
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: SizedBox(
+        width: double.infinity,
+        child: Column(
+          children: [
+            Expanded(
+              flex: 3,
+              child: PageView.builder(
+                itemBuilder: (
+                  ctx,
+                  index,
+                ) =>
+                    SplashContent(
+                  image: splashData[index]["image"],
+                  text: splashData[index]["text"],
+                ),
+                onPageChanged: (index) {
+                  setState(() {
+                    currentPage = index;
+                  });
+                },
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: Padding(
+                padding: kPaddingAllMedium,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
