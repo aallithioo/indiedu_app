5072b0ddc359f688b95cc838be59a463549affbe
Thio Alli <connect.thioalli@gmail.com>
2022-01-19 14:13:03 +0700

    refactor : size.dadrt

--- lib/src/app/themes/size.dart
+++ lib/src/app/themes/size.dart
@@ -1,5 +1,36 @@
+import 'package:flutter/material.dart';
+
 double kSizeTiny = 8.0;
 double kSizeSmall = 16.0;
 double kSizeMedium = 24.0;
 double kSizeLarge = 32.0;
 double kSizeHuge = 64.0;
+
+class SizeConfig {
+  static late MediaQueryData _mediaQueryData;
+  static late double screenWidth;
+  static late double screenHeight;
+  static double? defaultSize;
+  static Orientation? orientation;
+
+  void init(BuildContext context) {
+    _mediaQueryData = MediaQuery.of(context);
+    screenWidth = _mediaQueryData.size.width;
+    screenHeight = _mediaQueryData.size.height;
+    orientation = _mediaQueryData.orientation;
+  }
+}
+
+// Get the proportionate height as per screen size
+double getProportionateScreenHeight(double inputHeight) {
+  double screenHeight = SizeConfig.screenHeight;
+  // 812 is the layout height that designer use
+  return (inputHeight / 812.0) * screenHeight;
+}
+
+// Get the proportionate height as per screen size
+double getProportionateScreenWidth(double inputWidth) {
+  double screenWidth = SizeConfig.screenWidth;
+  // 375 is the layout width that designer use
+  return (inputWidth / 375.0) * screenWidth;
+}
