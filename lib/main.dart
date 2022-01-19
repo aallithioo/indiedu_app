9eb71b9344f1474ae95acfba27b8397104fffc04
Thio Alli <connect.thioalli@gmail.com>
2022-01-19 15:47:11 +0700

    feat : route - main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'src/app/routes/route.dart';
 
 import 'src/screens/intro/intro_screen.dart';
+import 'src/screens/sign_in/sign_in_screen.dart';
 import 'src/screens/splash/splash_screen.dart';
 
 void main() => runApp(const MyApp());
