81cd93a7cc526bf1fd031608a9787f9d19b80abf
Thio Alli <connect.thioalli@gmail.com>
2022-01-19 15:46:51 +0700

    feat : sign_in_screen.dart

---
+++ lib/src/screens/sign_in/widgets/sign_in_form.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class SignInForm extends StatefulWidget {
+  const SignInForm({Key? key}) : super(key: key);
+
+  @override
+  _SignInFormState createState() => _SignInFormState();
+}
+
+class _SignInFormState extends State<SignInForm> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final List<String?> errors = [];
+
+  void addError({String? error}) {
+    if (!errors.contains(error)) {
+      setState(() {
+        errors.add(error);
+      });
+    }
+  }
+
+  void removeError({String? error}) {
+    if (errors.contains(error)) {
+      setState(() {
+        errors.remove(error);
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      child: Column(),
+    );
+  }
+}
