58e93f6c4c23ad81014d76d4363d961366e999e0
Thio Alli <connect.thioalli@gmail.com>
2022-01-19 15:26:16 +0700

    feat : sign_in_screen.dart

---
+++ lib/src/screens/sign_in/sign_in_screen.dart
@@ -0,0 +1,14 @@
+import 'package:aallithioo/src/app/widgets/custom_appbar.dart';
+import 'package:flutter/material.dart';
+
+class SignInScreen extends StatelessWidget {
+  const SignInScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: kAppBar('Sign In'),
+      // body:
+    );
+  }
+}
