2f0c4c7ec5b50655768cc5a7e86ea83876f3a3ad
Thio Alli <connect.thioalli@gmail.com>
2022-01-19 15:21:30 +0700

    refactor : splash/

--- lib/src/screens/splash/splash_screen.dart
+++ lib/src/screens/splash/splash_screen.dart
@@ -1,10 +1,21 @@
+import 'package:aallithioo/src/screens/splash/widgets/splash_body.dart';
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({Key? key}) : super(key: key);
 
   @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 3), () {
+      Navigator.pushReplacementNamed(ctx, '/home');
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return const Scaffold(
+      body: SplashBody(),
+    );
   }
 }
---
+++ lib/src/screens/splash/widgets/splash_content.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class SplashContent extends StatelessWidget {
+  const SplashContent({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width,
+      height: 100,
+      child: const Image(
+        image: AssetImage(
+          "assets/images/png/img_dummy_1.png",
+        ),
+      ),
+    );
+  }
+}
