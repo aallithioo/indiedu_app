54f4fc79c089489fb970260cc6eef3ab2cfdba72
Thio Alli <connect.thioalli@gmail.com>
2022-01-20 10:54:28 +0700

    feat : body - sign_up_auth_body.dart

---
+++ lib/src/screens/sign_up/screens/auth/widgets/sign_up_auth_body.dart
@@ -0,0 +1,16 @@
+import 'package:aallithioo/src/app/widgets/custom_padding.dart';
+import 'package:flutter/material.dart';
+
+class SignUpAuthBody extends StatelessWidget {
+  const SignUpAuthBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: kPaddingSymetricHorizontalLarge,
+        child: Column(),
+      ),
+    );
+  }
+}
