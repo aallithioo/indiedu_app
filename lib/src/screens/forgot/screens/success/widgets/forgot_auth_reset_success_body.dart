149751feb14db007bd9b9a863e4c5958756a3193
Thio Alli <connect.thioalli@gmail.com>
2022-01-20 14:31:49 +0700

    feat : body - forgot_auth_reset_success_body.dart

--- lib/src/screens/forgot/screens/success/widgets/forgot_auth_reset_success_body.dart
+++ lib/src/screens/forgot/screens/success/widgets/forgot_auth_reset_success_body.dart
@@ -1,4 +1,12 @@
+import 'package:aallithioo/src/app/routes/route.dart';
+import 'package:aallithioo/src/app/themes/color.dart';
+import 'package:aallithioo/src/app/themes/fontweight.dart';
+import 'package:aallithioo/src/app/themes/size.dart';
+import 'package:aallithioo/src/app/themes/textalign.dart';
+import 'package:aallithioo/src/app/themes/theme.dart';
+import 'package:aallithioo/src/app/widgets/custom_border.dart';
 import 'package:aallithioo/src/app/widgets/custom_padding.dart';
+import 'package:aallithioo/src/app/widgets/custom_sizebox.dart';
 import 'package:flutter/material.dart';
 
 class ForgotAuthResetSuccessBody extends StatelessWidget {
@@ -9,6 +17,64 @@ class ForgotAuthResetSuccessBody extends StatelessWidget {
     return SafeArea(
       child: Padding(
         padding: kPaddingSymetricHorizontalLarge,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Spacer(),
+              SizedBox(
+                width: 200,
+                height: 200,
+                child: Image.asset('assets/images/png/img_dummy_0.png'),
+              ),
+              kSizeBoxVerticalLarge,
+              Container(
+                margin: EdgeInsets.fromLTRB(0, kSizeTiny, 0, 0),
+                child: Text(
+                  'Congratulations..',
+                  style: tooko.textTheme.headline5!.copyWith(
+                    color: kGreyColorShade900,
+                    fontWeight: kFontWeightSemiBold,
+                  ),
+                ),
+              ),
+              // Description subtitle
+              Container(
+                margin: EdgeInsets.fromLTRB(0, kSizeSmall, 0, kSizeLarge),
+                padding: kPaddingSymetricHorizontalMedium,
+                child: Text(
+                  'Your account password has been changed.',
+                  style: tooko.textTheme.bodyText1!.copyWith(
+                    color: tooko.textTheme.bodyText1!.color!.withOpacity(0.7),
+                  ),
+                  textAlign: kTextAlignCenter,
+                ),
+              ),
+              const Spacer(),
+              Container(
+                width: double.infinity,
+                height: 60,
+                decoration: BoxDecoration(
+                  color: kBlueColorShade400,
+                  borderRadius: kBorderRadiusTiny,
+                ),
+                child: TextButton(
+                  onPressed: () {
+                    Navigator.pushReplacementNamed(context, Routes.signIn);
+                  },
+                  child: Text(
+                    'Continue',
+                    style: tooko.textTheme.button!.copyWith(
+                      color: kGreyColorShade50,
+                    ),
+                  ),
+                ),
+              ),
+              kSizeBoxVerticalMedium,
+            ],
+          ),
+        ),
       ),
     );
   }
