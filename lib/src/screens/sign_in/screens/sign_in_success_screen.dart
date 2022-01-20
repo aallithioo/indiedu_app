7a1f71064d38db27ef150e7ed2a09074ffabe2fc
Thio Alli <connect.thioalli@gmail.com>
2022-01-20 07:58:51 +0700

    refactor : sig_in_success_screen.dart

--- lib/src/screens/sign_in/screens/sign_in_success_screen.dart
+++ lib/src/screens/sign_in/screens/sign_in_success_screen.dart
@@ -5,6 +5,8 @@ class SignInSuccessScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SignInSuccessBody(),
+    );
   }
 }
