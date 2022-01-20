94115ae56a9660893d767035ea019abda22c6a58
Thio Alli <connect.thioalli@gmail.com>
2022-01-20 09:05:54 +0700

    feat : form - sign_up_form.dart

--- lib/src/screens/sign_up/widgets/sign_up_form.dart
+++ lib/src/screens/sign_up/widgets/sign_up_form.dart
@@ -23,6 +23,7 @@ class _SignUpFormState extends State<SignUpForm> {
   final TextEditingController passwordController = TextEditingController();
   static bool isObscured = true;
 
+  @override
   Widget build(BuildContext context) {
     return Center(
       child: Form(
@@ -33,163 +34,167 @@ class _SignUpFormState extends State<SignUpForm> {
           children: [
             // Third party login
             buildThirdPartyButton(),
-            // Name
-            Container(
-              margin: EdgeInsets.fromLTRB(0, kSizeSmall, 0, 0),
-              width: double.infinity,
-              child: TextFormField(
-                controller: emailController,
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: kWhiteColorShade900,
-                  hintText: 'Name',
-                  hintStyle: tooko.textTheme.bodyText1!.copyWith(
-                    color: kGreyColorShade300,
-                    fontWeight: kFontWeightLight,
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: kBorderRadiusSmall,
-                    borderSide: BorderSide.none,
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: kBorderRadiusSmall,
-                    borderSide: BorderSide(
-                      color: kBlueColorShade400,
+            Column(
+              children: [
+                // Name
+                Container(
+                  margin: EdgeInsets.fromLTRB(0, kSizeSmall, 0, 0),
+                  width: double.infinity,
+                  child: TextFormField(
+                    controller: emailController,
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: kWhiteColorShade900,
+                      hintText: 'Name',
+                      hintStyle: tooko.textTheme.bodyText1!.copyWith(
+                        color: kGreyColorShade300,
+                        fontWeight: kFontWeightLight,
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: kBorderRadiusSmall,
+                        borderSide: BorderSide.none,
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: kBorderRadiusSmall,
+                        borderSide: BorderSide(
+                          color: kBlueColorShade400,
+                        ),
+                      ),
+                      prefixIcon: Icon(
+                        Icons.person_rounded,
+                        color: kGreyColorShade400,
+                      ),
                     ),
-                  ),
-                  prefixIcon: Icon(
-                    Icons.person_rounded,
-                    color: kGreyColorShade400,
+                    cursorColor: kBlueColorShade400,
                   ),
                 ),
-                cursorColor: kBlueColorShade400,
-              ),
-            ),
-            // Email
-            Container(
-              margin: EdgeInsets.fromLTRB(0, kSizeSmall, 0, 0),
-              width: double.infinity,
-              child: TextFormField(
-                controller: emailController,
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: kWhiteColorShade900,
-                  hintText: 'Email Address',
-                  hintStyle: tooko.textTheme.bodyText1!.copyWith(
-                    color: kGreyColorShade300,
-                    fontWeight: kFontWeightLight,
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: kBorderRadiusSmall,
-                    borderSide: BorderSide.none,
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: kBorderRadiusSmall,
-                    borderSide: BorderSide(
-                      color: kBlueColorShade400,
+                // Email
+                Container(
+                  margin: EdgeInsets.fromLTRB(0, kSizeSmall, 0, 0),
+                  width: double.infinity,
+                  child: TextFormField(
+                    controller: emailController,
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: kWhiteColorShade900,
+                      hintText: 'Email Address',
+                      hintStyle: tooko.textTheme.bodyText1!.copyWith(
+                        color: kGreyColorShade300,
+                        fontWeight: kFontWeightLight,
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: kBorderRadiusSmall,
+                        borderSide: BorderSide.none,
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: kBorderRadiusSmall,
+                        borderSide: BorderSide(
+                          color: kBlueColorShade400,
+                        ),
+                      ),
+                      prefixIcon: Icon(
+                        Icons.email_rounded,
+                        color: kGreyColorShade400,
+                      ),
                     ),
-                  ),
-                  prefixIcon: Icon(
-                    Icons.email_rounded,
-                    color: kGreyColorShade400,
+                    cursorColor: kBlueColorShade400,
                   ),
                 ),
-                cursorColor: kBlueColorShade400,
-              ),
-            ),
-            // Password
-            Container(
-              margin: EdgeInsets.fromLTRB(0, kSizeSmall, 0, 0),
-              width: MediaQuery.of(context).size.width,
-              child: TextFormField(
-                obscureText: isObscured,
-                obscuringCharacter: '•',
-                controller: passwordController,
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: kWhiteColorShade900,
-                  hintText: 'Password',
-                  hintStyle: tooko.textTheme.bodyText1!.copyWith(
-                    color: kGreyColorShade300,
-                    fontWeight: kFontWeightLight,
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: kBorderRadiusSmall,
-                    borderSide: BorderSide.none,
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: kBorderRadiusSmall,
-                    borderSide: BorderSide(
-                      color: kBlueColorShade500,
-                    ),
-                  ),
-                  prefixIcon: Icon(
-                    Icons.vpn_key_rounded,
-                    color: kGreyColorShade400,
-                  ),
-                  suffixIcon: IconButton(
-                    color: kGreyColorShade400,
-                    icon: Icon(
-                      isObscured ? Icons.visibility : Icons.visibility_off,
-                    ),
-                    onPressed: () {
-                      setState(
-                        () {
-                          isObscured = !isObscured;
+                // Password
+                Container(
+                  margin: EdgeInsets.fromLTRB(0, kSizeSmall, 0, 0),
+                  width: MediaQuery.of(context).size.width,
+                  child: TextFormField(
+                    obscureText: isObscured,
+                    obscuringCharacter: '•',
+                    controller: passwordController,
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: kWhiteColorShade900,
+                      hintText: 'Password',
+                      hintStyle: tooko.textTheme.bodyText1!.copyWith(
+                        color: kGreyColorShade300,
+                        fontWeight: kFontWeightLight,
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: kBorderRadiusSmall,
+                        borderSide: BorderSide.none,
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: kBorderRadiusSmall,
+                        borderSide: BorderSide(
+                          color: kBlueColorShade500,
+                        ),
+                      ),
+                      prefixIcon: Icon(
+                        Icons.vpn_key_rounded,
+                        color: kGreyColorShade400,
+                      ),
+                      suffixIcon: IconButton(
+                        color: kGreyColorShade400,
+                        icon: Icon(
+                          isObscured ? Icons.visibility : Icons.visibility_off,
+                        ),
+                        onPressed: () {
+                          setState(
+                            () {
+                              isObscured = !isObscured;
+                            },
+                          );
                         },
-                      );
-                    },
+                      ),
+                    ),
+                    cursorColor: kBlueColorShade500,
                   ),
                 ),
-                cursorColor: kBlueColorShade500,
-              ),
-            ),
-            // Confirm Password
-            Container(
-              margin: EdgeInsets.fromLTRB(0, kSizeSmall, 0, 0),
-              width: MediaQuery.of(context).size.width,
-              child: TextFormField(
-                obscureText: isObscured,
-                obscuringCharacter: '•',
-                controller: passwordController,
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: kWhiteColorShade900,
-                  hintText: 'Retype Password',
-                  hintStyle: tooko.textTheme.bodyText1!.copyWith(
-                    color: kGreyColorShade300,
-                    fontWeight: kFontWeightLight,
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: kBorderRadiusSmall,
-                    borderSide: BorderSide.none,
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: kBorderRadiusSmall,
-                    borderSide: BorderSide(
-                      color: kBlueColorShade500,
-                    ),
-                  ),
-                  prefixIcon: Icon(
-                    Icons.vpn_key_rounded,
-                    color: kGreyColorShade400,
-                  ),
-                  suffixIcon: IconButton(
-                    color: kGreyColorShade400,
-                    icon: Icon(
-                      isObscured ? Icons.visibility : Icons.visibility_off,
-                    ),
-                    onPressed: () {
-                      setState(
-                        () {
-                          isObscured = !isObscured;
+                // Confirm Password
+                Container(
+                  margin: EdgeInsets.fromLTRB(0, kSizeSmall, 0, 0),
+                  width: MediaQuery.of(context).size.width,
+                  child: TextFormField(
+                    obscureText: isObscured,
+                    obscuringCharacter: '•',
+                    controller: passwordController,
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: kWhiteColorShade900,
+                      hintText: 'Retype Password',
+                      hintStyle: tooko.textTheme.bodyText1!.copyWith(
+                        color: kGreyColorShade300,
+                        fontWeight: kFontWeightLight,
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: kBorderRadiusSmall,
+                        borderSide: BorderSide.none,
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: kBorderRadiusSmall,
+                        borderSide: BorderSide(
+                          color: kBlueColorShade500,
+                        ),
+                      ),
+                      prefixIcon: Icon(
+                        Icons.vpn_key_rounded,
+                        color: kGreyColorShade400,
+                      ),
+                      suffixIcon: IconButton(
+                        color: kGreyColorShade400,
+                        icon: Icon(
+                          isObscured ? Icons.visibility : Icons.visibility_off,
+                        ),
+                        onPressed: () {
+                          setState(
+                            () {
+                              isObscured = !isObscured;
+                            },
+                          );
                         },
-                      );
-                    },
+                      ),
+                    ),
+                    cursorColor: kBlueColorShade500,
                   ),
                 ),
-                cursorColor: kBlueColorShade500,
-              ),
+              ],
             ),
             // Button
           ],
