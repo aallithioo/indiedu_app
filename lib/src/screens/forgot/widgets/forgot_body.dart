6fb2298fd803efc55a10b0105d5637098a564f59
Thio Alli <connect.thioalli@gmail.com>
2022-01-20 12:04:16 +0700

    add : forgot_body.dart

---
+++ lib/src/screens/forgot/widgets/forgot_body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ForgotBody extends StatelessWidget {
+  const ForgotBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
