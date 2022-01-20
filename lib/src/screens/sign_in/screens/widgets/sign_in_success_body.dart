862001c1bdbef48384eae25d19c8d3268387fb34
Thio Alli <connect.thioalli@gmail.com>
2022-01-20 08:34:34 +0700

    feat : button - sign_in_success_body.dart

--- lib/src/screens/sign_in/screens/widgets/sign_in_success_body.dart
+++ lib/src/screens/sign_in/screens/widgets/sign_in_success_body.dart
@@ -1,4 +1,8 @@
+import 'package:aallithioo/src/app/themes/color.dart';
+import 'package:aallithioo/src/app/themes/theme.dart';
+import 'package:aallithioo/src/app/widgets/custom_border.dart';
 import 'package:aallithioo/src/app/widgets/custom_padding.dart';
+import 'package:aallithioo/src/app/widgets/custom_sizebox.dart';
 import 'package:flutter/material.dart';
 
 class SignInSuccessBody extends StatelessWidget {
@@ -14,11 +18,36 @@ class SignInSuccessBody extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
+              const Spacer(),
               SizedBox(
                 width: 200,
                 height: 200,
                 child: Image.asset('assets/images/png/img_dummy_0.png'),
               ),
+              kSizeBoxVerticalLarge,
+              Text(
+                'Sign in success',
+                style: tooko.textTheme.bodyText1,
+              ),
+              const Spacer(),
+              Container(
+                width: double.infinity,
+                height: 60,
+                decoration: BoxDecoration(
+                  color: kBlueColorShade400,
+                  borderRadius: kBorderRadiusTiny,
+                ),
+                child: TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Continue',
+                    style: tooko.textTheme.button!.copyWith(
+                      color: kGreyColorShade50,
+                    ),
+                  ),
+                ),
+              ),
+              kSizeBoxVerticalMedium,
             ],
           ),
         ),
