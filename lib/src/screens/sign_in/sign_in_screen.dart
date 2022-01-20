d663d26741c20e5983433ed378afca26b17354a3
Thio Alli <connect.thioalli@gmail.com>
2022-01-20 12:09:24 +0700

    refactor : sign_in_screen.dart

--- lib/src/screens/sign_in/sign_in_screen.dart
+++ lib/src/screens/sign_in/sign_in_screen.dart
@@ -9,7 +9,9 @@ class SignInScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: SignInBody(),
+      body: Center(
+        child: SignInBody(),
+      ),
     );
   }
 }
