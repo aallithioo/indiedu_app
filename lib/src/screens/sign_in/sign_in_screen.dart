99c4b415e16e8962ec343a3aa925ea5194dd6326
Thio Alli <connect.thioalli@gmail.com>
2022-01-20 12:09:03 +0700

    refactor : sign_in_screen.dart

--- lib/src/screens/sign_in/sign_in_screen.dart
+++ lib/src/screens/sign_in/sign_in_screen.dart
@@ -8,9 +8,8 @@ class SignInScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: kAppBar('Sign In'),
-      body: const SignInBody(),
+    return const Scaffold(
+      body: SignInBody(),
     );
   }
 }
