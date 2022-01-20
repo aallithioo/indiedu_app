cc53ad552c6799a3160dcfdbcc4cd3fbe310dfd8
Thio Alli <connect.thioalli@gmail.com>
2022-01-20 13:45:11 +0700

    add : forgot_auth_reset_success_screen.dart

---
+++ lib/src/screens/forgot/screens/reset/forgot_auth_reset_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ForgotAuthResetScreen extends StatelessWidget {
+  const ForgotAuthResetScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ForgotAuthResetBody(),
+    );
+  }
+}
