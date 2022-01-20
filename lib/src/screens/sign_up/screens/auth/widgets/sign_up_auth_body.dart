b6f7e0fca03ad3e9adcb189b6e3d36231f7824b9
Thio Alli <connect.thioalli@gmail.com>
2022-01-20 11:34:39 +0700

    feat : button - sign_up_auth.dart

--- lib/src/screens/sign_up/screens/auth/widgets/sign_up_auth_body.dart
+++ lib/src/screens/sign_up/screens/auth/widgets/sign_up_auth_body.dart
@@ -1,9 +1,13 @@
+import 'package:aallithioo/src/app/routes/route.dart';
 import 'package:aallithioo/src/app/themes/color.dart';
 import 'package:aallithioo/src/app/themes/fontweight.dart';
 import 'package:aallithioo/src/app/themes/size.dart';
+import 'package:aallithioo/src/app/themes/textalign.dart';
 import 'package:aallithioo/src/app/themes/theme.dart';
+import 'package:aallithioo/src/app/widgets/custom_blur.dart';
 import 'package:aallithioo/src/app/widgets/custom_border.dart';
 import 'package:aallithioo/src/app/widgets/custom_padding.dart';
+import 'package:aallithioo/src/app/widgets/custom_sizebox.dart';
 import 'package:flutter/material.dart';
 
 class SignUpAuthBody extends StatefulWidget {
@@ -22,6 +26,8 @@ class _SignUpAuthBodyState extends State<SignUpAuthBody> {
       child: Padding(
         padding: kPaddingSymetricHorizontalLarge,
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             // Image
             SizedBox(
@@ -29,10 +35,31 @@ class _SignUpAuthBodyState extends State<SignUpAuthBody> {
               height: 200,
               child: Image.asset('assets/images/png/img_dummy_0.png'),
             ),
+            // Text
+            Container(
+              margin: EdgeInsets.fromLTRB(0, kSizeMedium, 0, 0),
+              child: Text(
+                'Verify your account',
+                style: tooko.textTheme.headline5!.copyWith(
+                  color: kGreyColorShade900,
+                  fontWeight: kFontWeightSemiBold,
+                ),
+              ),
+            ),
+            // Description subtitle
+            Container(
+              margin: EdgeInsets.fromLTRB(0, kSizeSmall, 0, kSizeSmall),
+              padding: kPaddingSymetricHorizontalMedium,
+              child: Text(
+                'Please enter your verification code to continue.',
+                style: tooko.textTheme.bodyText1!.copyWith(),
+                textAlign: kTextAlignCenter,
+              ),
+            ),
             // Form Field
             Form(
               child: Container(
-                margin: EdgeInsets.fromLTRB(0, kSizeSmall, 0, 0),
+                margin: EdgeInsets.fromLTRB(0, kSizeSmall, 0, kSizeSmall),
                 width: double.infinity,
                 child: TextFormField(
                   controller: tokenController,
@@ -60,7 +87,45 @@ class _SignUpAuthBodyState extends State<SignUpAuthBody> {
               ),
             ),
             // Button
+            Container(
+              margin: EdgeInsets.fromLTRB(0, kSizeSmall, 0, 0),
+              width: MediaQuery.of(context).size.width,
+              height: 60,
+              decoration: BoxDecoration(
+                borderRadius: kBorderRadiusSmall,
+                color: kBlueColorShade400,
+                boxShadow: [
+                  BoxShadow(
+                    color: kWhiteColorShade800,
+                    blurRadius: kBlurRadiusHuge,
+                    offset: const Offset(0, 5),
+                  ),
+                ],
+              ),
+              child: TextButton(
+                onPressed: () {
+                  Navigator.pushReplacementNamed(context, Routes.signUpSuccess);
+                },
+                child: Text(
+                  'Verify Account',
+                  style: tooko.textTheme.button!.copyWith(
+                    color: kWhiteColorShade900,
+                  ),
+                ),
+              ),
+            ),
             // Button
+            TextButton(
+              onPressed: () {
+                Navigator.pushReplacementNamed(context, Routes.signUpSuccess);
+              },
+              child: Text(
+                'Resend Verification Code',
+                style: tooko.textTheme.button!.copyWith(
+                  color: kBlueColorShade400,
+                ),
+              ),
+            ),
           ],
         ),
       ),
