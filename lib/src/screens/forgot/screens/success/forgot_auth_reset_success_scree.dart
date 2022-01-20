71a7693318c54d66e98d5c465b8f6ca099164808
Thio Alli <connect.thioalli@gmail.com>
2022-01-20 14:29:21 +0700

    feat : body - forgot_auth_reset_success_screen.dart

---
+++ lib/src/screens/forgot/screens/success/forgot_auth_reset_success_scree.dart
@@ -0,0 +1,13 @@
+import 'package:aallithioo/src/screens/forgot/screens/success/widgets/forgot_auth_reset_success_body.dart';
+import 'package:flutter/material.dart';
+
+class ForgotAuthResetSuccessScreen extends StatelessWidget {
+  const ForgotAuthResetSuccessScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: ForgotAuthResetSuccessBody(),
+    );
+  }
+}
