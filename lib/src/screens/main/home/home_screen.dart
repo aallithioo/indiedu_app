14d69fcbc8089f13b20828261cfaa0c2b9c31750
Thio Alli <connect.thioalli@gmail.com>
2022-01-21 15:06:47 +0700

    feat : body - home_screen.dart

--- lib/src/screens/main/home/home_screen.dart
+++ lib/src/screens/main/home/home_screen.dart
@@ -1,3 +1,9 @@
+import '../../../app/enums.dart';
+
+import '../../../app/widgets/custom_bottom_nav_bar.dart';
+
+import 'widgets/home_body.dart';
+
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -5,6 +11,9 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Scaffold(
+      body: HomeBody(),
+      bottomNavigationBar: CustomBottomNavBar(selectedMenu: MenuState.home),
+    );
   }
 }
