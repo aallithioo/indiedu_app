f0c2db9e7934b154efa884c0ce45af1f890bfcd3
Thio Alli <connect.thioalli@gmail.com>
2022-01-21 11:04:51 +0700

    minor update

--- lib/src/screens/forgot/screens/success/widgets/forgot_auth_reset_success_body.dart
+++ lib/src/screens/forgot/screens/success/widgets/forgot_auth_reset_success_body.dart
@@ -21,63 +21,61 @@ class ForgotAuthResetSuccessBody extends StatelessWidget {
       child: Padding(
         padding: kPaddingSymetricHorizontalLarge,
         child: Center(
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const Spacer(),
-                SizedBox(
-                  width: 200,
-                  height: 200,
-                  child: Image.asset('assets/images/png/img_dummy_0.png'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Spacer(),
+              SizedBox(
+                width: 200,
+                height: 200,
+                child: Image.asset('assets/images/png/img_dummy_0.png'),
+              ),
+              kSizeBoxVerticalLarge,
+              Container(
+                margin: EdgeInsets.fromLTRB(0, kSizeTiny, 0, 0),
+                child: Text(
+                  'Congratulations..',
+                  style: tooko.textTheme.headline5!.copyWith(
+                    color: kGreyColorShade900,
+                    fontWeight: kFontWeightSemiBold,
+                  ),
                 ),
-                kSizeBoxVerticalLarge,
-                Container(
-                  margin: EdgeInsets.fromLTRB(0, kSizeTiny, 0, 0),
+              ),
+              // Description subtitle
+              Container(
+                margin: EdgeInsets.fromLTRB(0, kSizeSmall, 0, kSizeLarge),
+                padding: kPaddingSymetricHorizontalMedium,
+                child: Text(
+                  'Your account password has been changed.',
+                  style: tooko.textTheme.bodyText1!.copyWith(
+                    color: tooko.textTheme.bodyText1!.color!.withOpacity(0.7),
+                  ),
+                  textAlign: kTextAlignCenter,
+                ),
+              ),
+              const Spacer(),
+              Container(
+                width: double.infinity,
+                height: 60,
+                decoration: BoxDecoration(
+                  color: kBlueColorShade400,
+                  borderRadius: kBorderRadiusTiny,
+                ),
+                child: TextButton(
+                  onPressed: () {
+                    Navigator.pushReplacementNamed(context, Routes.signIn);
+                  },
                   child: Text(
-                    'Congratulations..',
-                    style: tooko.textTheme.headline5!.copyWith(
-                      color: kGreyColorShade900,
-                      fontWeight: kFontWeightSemiBold,
+                    'Continue',
+                    style: tooko.textTheme.button!.copyWith(
+                      color: kGreyColorShade50,
                     ),
                   ),
                 ),
-                // Description subtitle
-                Container(
-                  margin: EdgeInsets.fromLTRB(0, kSizeSmall, 0, kSizeLarge),
-                  padding: kPaddingSymetricHorizontalMedium,
-                  child: Text(
-                    'Your account password has been changed.',
-                    style: tooko.textTheme.bodyText1!.copyWith(
-                      color: tooko.textTheme.bodyText1!.color!.withOpacity(0.7),
-                    ),
-                    textAlign: kTextAlignCenter,
-                  ),
-                ),
-                const Spacer(),
-                Container(
-                  width: double.infinity,
-                  height: 60,
-                  decoration: BoxDecoration(
-                    color: kBlueColorShade400,
-                    borderRadius: kBorderRadiusTiny,
-                  ),
-                  child: TextButton(
-                    onPressed: () {
-                      Navigator.pushReplacementNamed(context, Routes.signIn);
-                    },
-                    child: Text(
-                      'Continue',
-                      style: tooko.textTheme.button!.copyWith(
-                        color: kGreyColorShade50,
-                      ),
-                    ),
-                  ),
-                ),
-                kSizeBoxVerticalMedium,
-              ],
-            ),
+              ),
+              kSizeBoxVerticalMedium,
+            ],
           ),
         ),
       ),
