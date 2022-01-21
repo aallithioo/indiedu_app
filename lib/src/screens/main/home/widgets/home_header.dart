184d280a35eac25f22990997bdcdd698d61e43bb
Thio Alli <connect.thioalli@gmail.com>
2022-01-21 14:40:15 +0700

    add : home_header.dart

---
+++ lib/src/screens/main/home/widgets/home_header.dart
@@ -0,0 +1,20 @@
+import 'package:aallithioo/src/app/widgets/custom_padding.dart';
+import 'package:flutter/material.dart';
+
+class HomeHeader extends StatelessWidget {
+  const HomeHeader({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: kPaddingSymetricHorizontalMedium,
+      child: Row(
+        children: [
+          // TODO: Add search bar
+          // TODO: Add Icon
+          // TODO: Add Icon
+        ],
+      ),
+    );
+  }
+}
