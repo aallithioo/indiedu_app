7e14eb8056153912da2de0aa03b6d465c598b950
Thio Alli <connect.thioalli@gmail.com>
2022-01-24 06:28:05 +0700

    feat : search box - home_header.dart

--- lib/src/screens/main/home/widgets/home_header.dart
+++ lib/src/screens/main/home/widgets/home_header.dart
@@ -1,6 +1,7 @@
 import 'package:aallithioo/src/app/themes/color.dart';
 import 'package:aallithioo/src/app/widgets/custom_padding.dart';
 import 'package:aallithioo/src/screens/main/home/widgets/icon_with_counter.dart';
+import 'package:aallithioo/src/screens/main/home/widgets/search_field.dart';
 import 'package:flutter/material.dart';
 
 class HomeHeader extends StatelessWidget {
@@ -14,6 +15,7 @@ class HomeHeader extends StatelessWidget {
         child: Row(
           children: [
             // TODO: Add search bar
+            SearchField(),
             // TODO: Add Icon
             IconButtonWithCounter(
               svgSrc: "assets/icons/svg/Cart Icon.svg",
