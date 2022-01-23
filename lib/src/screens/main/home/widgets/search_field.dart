f924761057af464383282bd59069a81aaa41c00f
Thio Alli <connect.thioalli@gmail.com>
2022-01-24 06:02:26 +0700

    add : search_field.dart

---
+++ lib/src/screens/main/home/widgets/search_field.dart
@@ -0,0 +1,35 @@
+import 'package:aallithioo/src/app/themes/color.dart';
+import 'package:aallithioo/src/app/themes/size.dart';
+import 'package:aallithioo/src/app/widgets/custom_border.dart';
+import 'package:flutter/material.dart';
+
+class SearchField extends StatelessWidget {
+  const SearchField({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.6,
+      decoration: BoxDecoration(
+        color: kGreyColorShade300,
+        borderRadius: kBorderRadiusSmall,
+      ),
+      child: TextField(
+        onChanged: (value) => print(value),
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.symmetric(
+            horizontal: kSizeMedium,
+            vertical: kSizeTiny,
+          ),
+          border: InputBorder.none,
+          focusedBorder: InputBorder.none,
+          enabledBorder: InputBorder.none,
+          hintText: "Search Course",
+          prefixIcon: const Icon(
+            Icons.search,
+          ),
+        ),
+      ),
+    );
+  }
+}
