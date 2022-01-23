ad6e13d84573ccea65f6dee4471db86316cf8805
Thio Alli <connect.thioalli@gmail.com>
2022-01-24 06:27:25 +0700

    refactor : home_header.dart

--- lib/src/screens/main/home/widgets/home_header.dart
+++ lib/src/screens/main/home/widgets/home_header.dart
@@ -1,3 +1,4 @@
+import 'package:aallithioo/src/app/themes/color.dart';
 import 'package:aallithioo/src/app/widgets/custom_padding.dart';
 import 'package:aallithioo/src/screens/main/home/widgets/icon_with_counter.dart';
 import 'package:flutter/material.dart';
@@ -7,22 +8,24 @@ class HomeHeader extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: kPaddingSymetricHorizontalMedium,
-      child: Row(
-        children: [
-          // TODO: Add search bar
-          // TODO: Add Icon
-          IconButtonWithCounter(
-            svgSrc: "assets/icons/svg/Cart Icon.svg",
-            press: () {},
-          ),
-          // TODO: Add Icon
-          IconButtonWithCounter(
-            svgSrc: "assets/icons/svg/Bell.svg",
-            press: () {},
-          )
-        ],
+    return Container(
+      child: Padding(
+        padding: kPaddingSymetricHorizontalMedium,
+        child: Row(
+          children: [
+            // TODO: Add search bar
+            // TODO: Add Icon
+            IconButtonWithCounter(
+              svgSrc: "assets/icons/svg/Cart Icon.svg",
+              press: () {},
+            ),
+            // TODO: Add Icon
+            IconButtonWithCounter(
+              svgSrc: "assets/icons/svg/Bell.svg",
+              press: () {},
+            )
+          ],
+        ),
       ),
     );
   }
