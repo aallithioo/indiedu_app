5c81d7748e7a0ba896b1c0c1ef9f6900a77f6cba
Thio Alli <connect.thioalli@gmail.com>
2022-01-24 05:55:24 +0700

    add : section_title.dart

---
+++ lib/src/screens/main/home/widgets/section_title.dart
@@ -0,0 +1,28 @@
+import 'package:aallithioo/src/app/themes/theme.dart';
+import 'package:flutter/material.dart';
+
+class SectionTitle extends StatelessWidget {
+  const SectionTitle({
+    Key? key,
+    required this.title,
+    required this.onTap,
+  }) : super(key: key);
+
+  final String title;
+  final GestureTapCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          title,
+          style: tooko.textTheme.bodyText1!.copyWith(
+            color: tooko.textTheme.bodyText1!.color,
+          ),
+        ),
+      ],
+    );
+  }
+}
