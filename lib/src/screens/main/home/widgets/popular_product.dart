996cd8d94a9a06ba619946239e67c1d144b0e16f
Thio Alli <connect.thioalli@gmail.com>
2022-01-24 08:45:20 +0700

    feat : list - popular_product.dart

--- lib/src/screens/main/home/widgets/popular_product.dart
+++ lib/src/screens/main/home/widgets/popular_product.dart
@@ -1,4 +1,5 @@
 import 'package:aallithioo/src/app/widgets/custom_padding.dart';
+import 'package:aallithioo/src/app/widgets/custom_sizebox.dart';
 import 'package:aallithioo/src/screens/main/home/widgets/section_title.dart';
 import 'package:flutter/material.dart';
 
@@ -14,6 +15,25 @@ class PopularProduct extends StatelessWidget {
           child: SectionTitle(
             title: "Popular Course",
             onTap: () {},
+          ),
+        ),
+        kSizeBoxVerticalMedium,
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            children: [
+              ...List.generate(
+                demoProducts.length,
+                (index) {
+                  if (demoProducts[index].isPopular)
+                    return ProductCard(product: demoProducts[index]);
+
+                  return SizedBox
+                      .shrink(); // here by default width and height is 0
+                },
+              ),
+              kSizeBoxVerticalMedium,
+            ],
           ),
         ),
       ],
