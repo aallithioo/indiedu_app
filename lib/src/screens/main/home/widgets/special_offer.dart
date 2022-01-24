4c7da6676704effb70f91b141eb1468c4ef5c23e
Thio Alli <connect.thioalli@gmail.com>
2022-01-24 07:30:39 +0700

    feat : card - special_offer.dart

--- lib/src/screens/main/home/widgets/special_offer.dart
+++ lib/src/screens/main/home/widgets/special_offer.dart
@@ -1,3 +1,7 @@
+import 'package:aallithioo/src/app/themes/color.dart';
+import 'package:aallithioo/src/app/themes/size.dart';
+import 'package:aallithioo/src/app/themes/theme.dart';
+import 'package:aallithioo/src/app/widgets/custom_border.dart';
 import 'package:aallithioo/src/app/widgets/custom_padding.dart';
 import 'package:aallithioo/src/app/widgets/custom_sizebox.dart';
 import 'package:aallithioo/src/screens/main/home/widgets/section_title.dart';
@@ -25,6 +29,78 @@ class SpecialOffer extends StatelessWidget {
           ),
         ),
       ],
+    );
+  }
+}
+
+class SpecialOfferCard extends StatelessWidget {
+  const SpecialOfferCard({
+    Key? key,
+    required this.category,
+    required this.image,
+    required this.numOfBrands,
+    required this.press,
+  }) : super(key: key);
+
+  final String category, image;
+  final int numOfBrands;
+  final GestureTapCallback press;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: kPaddingSymetricHorizontalMedium,
+      child: GestureDetector(
+        onTap: press,
+        child: SizedBox(
+          width: kSizeHuge * 4,
+          height: kSizeHuge * 2,
+          child: ClipRRect(
+            borderRadius: kBorderRadiusSmall,
+            child: Stack(
+              children: [
+                Image.asset(
+                  image,
+                  fit: BoxFit.cover,
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [
+                        kBlueColorShade100.withOpacity(0.5),
+                        kBlueColorShade200.withOpacity(0.5),
+                      ],
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: kPaddingAllSmall,
+                  child: Text.rich(
+                    TextSpan(
+                      children: [
+                        TextSpan(
+                          text: "$category\n",
+                          style: tooko.textTheme.bodyText1!.copyWith(
+                            color: kGreyColorShade50,
+                          ),
+                        ),
+                        TextSpan(
+                          text: "$numOfBrands Brands",
+                          style: tooko.textTheme.bodyText1!.copyWith(
+                            color: kGreyColorShade50,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
