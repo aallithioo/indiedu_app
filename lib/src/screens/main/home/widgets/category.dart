28414acb462fdc9ec15daec7a6ccdf4bf9cedcb0
Thio Alli <connect.thioalli@gmail.com>
2022-01-24 08:26:51 +0700

    add : category.datt

---
+++ lib/src/screens/main/home/widgets/category.dart
@@ -0,0 +1,18 @@
+import 'package:aallithioo/src/app/widgets/custom_padding.dart';
+import 'package:flutter/material.dart';
+
+class CategoryCard extends StatelessWidget {
+  const CategoryCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: kPaddingAllSmall,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [],
+      ),
+    );
+  }
+}
