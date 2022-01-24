0d3e0c576242dc9024e75090246f2c987f69cc03
Thio Alli <connect.thioalli@gmail.com>
2022-01-24 07:20:51 +0700

    add : special_offer.dart

--- lib/src/screens/main/home/widgets/special_offer.dart
+++ lib/src/screens/main/home/widgets/special_offer.dart
@@ -1,9 +1,10 @@
 import 'package:aallithioo/src/app/widgets/custom_padding.dart';
+import 'package:aallithioo/src/app/widgets/custom_sizebox.dart';
 import 'package:aallithioo/src/screens/main/home/widgets/section_title.dart';
 import 'package:flutter/material.dart';
 
-class PopularProduct extends StatelessWidget {
-  const PopularProduct({Key? key}) : super(key: key);
+class SpecialOffer extends StatelessWidget {
+  const SpecialOffer({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -12,8 +13,15 @@ class PopularProduct extends StatelessWidget {
         Padding(
           padding: kPaddingSymetricHorizontalMedium,
           child: SectionTitle(
-            title: "Popular Course",
+            title: 'Special For You',
             onTap: () {},
+          ),
+        ),
+        kSizeBoxVerticalMedium,
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            children: [],
           ),
         ),
       ],
