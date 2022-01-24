12f5cd31e8adf7f542c0ab867aaafabf7d0dbb44
Thio Alli <connect.thioalli@gmail.com>
2022-01-24 08:32:41 +0700

    feat : card - category.dart

--- lib/src/screens/main/home/widgets/category.dart
+++ lib/src/screens/main/home/widgets/category.dart
@@ -1,5 +1,11 @@
+import 'package:aallithioo/src/app/themes/color.dart';
+import 'package:aallithioo/src/app/themes/size.dart';
+import 'package:aallithioo/src/app/widgets/custom_border.dart';
+import 'package:aallithioo/src/app/widgets/custom_margin.dart';
 import 'package:aallithioo/src/app/widgets/custom_padding.dart';
+import 'package:aallithioo/src/app/widgets/custom_sizebox.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class CategoryCard extends StatelessWidget {
   const CategoryCard({Key? key}) : super(key: key);
@@ -12,6 +18,47 @@ class CategoryCard extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [],
+      ),
+    );
+  }
+}
+
+class CatergoryCardItem extends StatelessWidget {
+  const CatergoryCardItem({
+    Key? key,
+    required this.icon,
+    required this.text,
+    required this.press,
+  }) : super(key: key);
+
+  final String icon, text;
+  final GestureTapCallback press;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: press,
+      child: SizedBox(
+        width: kSizeLarge,
+        child: Column(
+          children: [
+            Container(
+              padding: kPaddingAllSmall,
+              height: kSizeLarge * 1.5,
+              width: kSizeLarge * 1.5,
+              decoration: BoxDecoration(
+                color: kGreyColorShade300,
+                borderRadius: kBorderRadiusSmall,
+              ),
+              child: SvgPicture.asset(icon),
+            ),
+            kSizeBoxVerticalTiny,
+            Text(
+              text,
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
       ),
     );
   }
