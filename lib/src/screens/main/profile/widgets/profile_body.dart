11e3b667132728fd5e4d083df34b292d88e7a6c6
Thio Alli <connect.thioalli@gmail.com>
2022-01-25 09:56:14 +0700

    minor update

--- lib/src/screens/main/profile/widgets/profile_body.dart
+++ lib/src/screens/main/profile/widgets/profile_body.dart
@@ -3,6 +3,8 @@ import 'package:aallithioo/src/app/widgets/custom_sizebox.dart';
 import 'package:aallithioo/src/screens/main/profile/widgets/profile_pic.dart';
 import 'package:flutter/material.dart';
 
+import 'profile_menu.dart';
+
 class ProfileBody extends StatelessWidget {
   const ProfileBody({Key? key}) : super(key: key);
 
@@ -16,6 +18,32 @@ class ProfileBody extends StatelessWidget {
             children: [
               kSizeBoxVerticalMedium,
               ProfilePic(),
+              SizedBox(height: 20),
+              ProfileMenu(
+                text: "My Account",
+                icon: "assets/icons/User Icon.svg",
+                press: () => {},
+              ),
+              ProfileMenu(
+                text: "Notifications",
+                icon: "assets/icons/Bell.svg",
+                press: () {},
+              ),
+              ProfileMenu(
+                text: "Settings",
+                icon: "assets/icons/Settings.svg",
+                press: () {},
+              ),
+              ProfileMenu(
+                text: "Help Center",
+                icon: "assets/icons/Question mark.svg",
+                press: () {},
+              ),
+              ProfileMenu(
+                text: "Log Out",
+                icon: "assets/icons/Log out.svg",
+                press: () {},
+              ),
             ],
           ),
         ),
