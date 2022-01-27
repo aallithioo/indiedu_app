a1cd5147b475242ce08d7a51f79a06e4d25a1a6e
Thio Alli <connect.thioalli@gmail.com>
2022-01-27 14:07:28 +0700

    refactor : background - sign_in_success_screendart

--- lib/src/screens/sign_in/screens/success/sign_in_success_screen.dart
+++ lib/src/screens/sign_in/screens/success/sign_in_success_screen.dart
@@ -2,13 +2,16 @@ import 'package:flutter/material.dart';
 
 import 'widgets/sign_in_success_body.dart';
 
+import '../../../../app/themes/color.dart';
+
 class SignInSuccessScreen extends StatelessWidget {
   const SignInSuccessScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SignInSuccessBody(),
+    return Scaffold(
+      backgroundColor: kWhiteColorShade900,
+      body: const SignInSuccessBody(),
     );
   }
 }
